ed00f5b78245ccc4e9e92edc6451afc1e95c20e5
Pedro Martins <pedro.rufino.martins@gmail.com>
2026-03-24 14:04:11 -0300

    initial commit and input data

---
+++ Processin_data.R
@@ -0,0 +1,6 @@
+
+data_raw =  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQx8IN6CoV4lbjPFXnjY5HDYpUbX0E-wqxjsfZ9oHHCqfTLz6wEmMUIMlGJS3eTXz20OO7PvvbxndvU/pub?output=csv"
+data_raw <- read.csv(data_raw, dec= ",")
+
+
+
