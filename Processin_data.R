a3f3729f85d9b203e6b9130e9a12287fe9cf7e2b
Pedro Martins <pedro.rufino.martins@gmail.com>
2026-03-24 16:29:29 -0300

    few changes

--- Processin_data.R
+++ Processin_data.R
@@ -23,10 +23,12 @@ library(wallace)
 library(utils)
 library(rmaxent)
 
+data_url =  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQx8IN6CoV4lbjPFXnjY5HDYpUbX0E-wqxjsfZ9oHHCqfTLz6wEmMUIMlGJS3eTXz20OO7PvvbxndvU/pub?output=csv"
+data_raw <- read.csv(data_url,
+                     dec= ",",
+                     header = T)
 
 
-data_raw =  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQx8IN6CoV4lbjPFXnjY5HDYpUbX0E-wqxjsfZ9oHHCqfTLz6wEmMUIMlGJS3eTXz20OO7PvvbxndvU/pub?output=csv"
-data_raw <- read.csv(data_raw, dec= ",")
 
 
 
