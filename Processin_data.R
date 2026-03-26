6435afbac0e7236203f22a051c17508c11f146f5
Pedro Martins <pedro.rufino.martins@gmail.com>
2026-03-26 18:06:56 -0300

    changes

--- Processin_data.R
+++ Processin_data.R
@@ -29,28 +29,33 @@ data_raw <- read.csv(data_url,
                      header = T)
 
 data_clean <- subset(data_raw,(!is.na(Presence)))
+data_clean$Biomassa_g <- as.numeric(
+  gsub(",", ".", gsub("\\.", "", data_clean$Biomassa_g))
+)
 
-crdref <- "+proj=longlat +datum=WGS84"
 
-data_presence <- data_clean [,c(1:4)]
-coordinates(data_presence) <- ~ LONG + LAT
-myCRS1 <- CRS("+proj=longlat +datum=WGS84") # WGS 84
-crs(data_presence) <- myCRS1
-data_coord <- cbind(data_clean$LONG, data_clean$LAT)
-data_coord <- vect(data_coord)
-crs(data_coord) <- "+proj=longlat +datum=WGS84"
+data_coord <- vect(data_clean [,-c(7:14)],
+                   geom = c("LONG", "LAT"),
+                   crs ="EPSG:4326")
+rast_tamplate <- rast(ext(data_coord),
+                      resolution = 0.0001,
+                      crs = crs(data_coord))
 
-data_val <- data_clean [, -c(1:4,7:14)]
-raster_val1 <- rast(ncol=ncol(data_val), nrow=nrow(data_val), xmin=-150, xmax=-80, ymin=20, ymax=60)
-raster_val2 <- rast(ncol=ncol(data_val), nrow=nrow(data_val), xmin=-150, xmax=-80, ymin=20, ymax=60)
-values(raster_val1) <- data_val [1]
-values(raster_val2) <- data_val [2]
-raster_val = c(raster_val1, raster_val2)
 
-occ_buffer <- terra::buffer(data_coord,width=4*10^5)#unit is meter
+data_lyer_n <- rasterize (data_coord,
+                           rast_tamplate,
+                           field = "N")
+data_lyer_biomassa <- rasterize(data_coord,
+                                rast_tamplate,
+                                field = "Biomassa_g")
+r_stack <- c(data_lyer_n,
+             data_lyer_biomassa)
+names(r_stack) <- c("N",
+                    "Biomassa_g")
 
-# raster_mask <- terra::mask(raster_val, occ_buffer)
-# cat (class (raster_val), class (data_presence))
-#
-# maxent(x=raster_val, p=data_presence)
+r_stack <- stack(r_stack)
+data_coord <- as(data_coord,
+                 "Spatial")
 
+maxent(r_stack,data_coord)
+class(data_coord)
